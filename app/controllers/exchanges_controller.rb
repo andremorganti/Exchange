ef1c0db2a4b20dfe45a3fdb0b0d56632470dc2ee
Andre Morganti <acmorganti@outlook.com.br>
2018-06-11 17:00:54 -0300

    teste deploy

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -5,6 +5,7 @@ class ExchangesController < ApplicationController
   def convert
     #api para enviar o form para conversão e devolver o json para ser capturado pelo
     #ajax e informar no campo result
+    #teste deploy
     value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
     render json: {"value": value}
   end
