d4a6a6a19dee71f32425f893fa0b657caa6d84be
Andre Morganti <acmorganti@outlook.com.br>
2018-06-12 10:48:34 -0300

    service para o bitcoin sservice

--- app/services/bitcoin_service.rb
+++ app/services/bitcoin_service.rb
@@ -1,21 +1,17 @@
 require 'rest-client'
 require 'json'
 
-class ExchangeService
-  def initialize(source_currency, target_currency, amount)
-    @source_currency = source_currency
-    @target_currency = target_currency
+class BitcoinService
+  def initialize(amount)
     @amount = amount.to_f
   end
 
  
   def perform
     begin
-      exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
-      exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
-      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
+      url = 'https://www.mercadobitcoin.net/api/BTC/ticker/' 
       res = RestClient.get url
-      value = JSON.parse(res.body)['currency'][0]['value'].to_f
+      value = JSON.parse(res.body)['ticker']['last'].to_f
       
       value * @amount
     rescue RestClient::ExceptionWithResponse => e
